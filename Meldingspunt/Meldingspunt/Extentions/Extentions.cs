6e956b382963d3bfd724860aaf2980c28b9f77f4
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2022-11-01 08:52:18 +0100

    Added Nuget

---
+++ Meldingspunt/Meldingspunt/Extentions/Extentions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meldingspunt.Extentions
+{
+    public class Extentions
+    {
+       
+            public static void DebugOutput(string text)
+            {
+
+                System.Diagnostics.Debug.WriteLine(text);
+            }
+
+  
+          
+        }
+    
+}
---
+++ Meldingspunt/Meldingspunt/Services/ServiceBaseCLass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Meldingspunt.Services
+{
+    public class ServiceBaseCLass
+    {
+        //connect
+
+        //Get
+
+        //Post
+    }
+    public void CreateConnection()
+    {
+        string connetionString = null;
+        SqlConnection cnn;
+        connetionString = "Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password"
+            cnn = new SqlConnection(connetionString);
+        try
+        {
+            cnn.Open();
+            Extentions.Extentions.DebugOutput("connection open");
+            cnn.Close();
+        }
+        catch (Exception ex)
+        {
+            Extentions.Extentions.DebugOutput("cannot open Connection");
+        }
+    }
+}
