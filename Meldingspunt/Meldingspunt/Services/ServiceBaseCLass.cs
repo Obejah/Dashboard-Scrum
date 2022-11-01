27a5ea02cf868aca442853030cb66f61b599e849
JammingEnd <57032230+JammingEnd@users.noreply.github.com>
2022-11-01 09:44:17 +0100

    fixed SqlCLient

--- Meldingspunt/Meldingspunt/Services/ServiceBaseCLass.cs
+++ Meldingspunt/Meldingspunt/Services/ServiceBaseCLass.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.Data.SqlClient;
 
 namespace Meldingspunt.Services
 {
@@ -13,22 +13,23 @@ namespace Meldingspunt.Services
         //Get
 
         //Post
-    }
-    public void CreateConnection()
-    {
-        string connetionString = null;
-        SqlConnection cnn;
-        connetionString = "Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password";
+        public void CreateConnection()
+        {
+            string connetionString = null;
+            SqlConnection cnn;
+            connetionString = "Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password";
             cnn = new SqlConnection(connetionString);
-        try
-        {
-            cnn.Open();
-            Extentions.Extentions.DebugOutput("connection open");
-            cnn.Close();
-        }
-        catch (Exception ex)
-        {
-            Extentions.Extentions.DebugOutput("cannot open Connection");
+            try
+            {
+                cnn.Open();
+                Extentions.Extentions.DebugOutput("connection open");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                Extentions.Extentions.DebugOutput("cannot open Connection");
+            }
         }
     }
+    
 }
